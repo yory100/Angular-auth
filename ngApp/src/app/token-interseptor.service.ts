0f8473256cb9a0367dbd260da90856e3f5c4adc3
Yordan Yordanov <yordan.yordanov@dormakaba.com>
2019-04-25 15:47:42 +0300

    token iterceptor done

--- ngApp/src/app/token-interseptor.service.ts
+++ ngApp/src/app/token-interseptor.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterseptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private _authService: AuthService) { }
 
   intercept(req, next) {
     let tokenizedReq = req.clone({
       setHeaders: {
-        Authorization: 'Bearer xx.yy.zz'
+        Authorization: `Bearer ${this._authService.getToken()}`
       }
     });
 
