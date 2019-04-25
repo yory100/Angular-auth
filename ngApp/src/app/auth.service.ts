2ec53fd9c9716cf66b38423ead5746ad96706696
Yordan Yordanov <yordan.yordanov@dormakaba.com>
2019-04-25 16:19:54 +0300

    final commit

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,10 @@ export class AuthService {
   private _registerUrl = 'http://localhost:3000/api/register';
   private _loginUrl = 'http://localhost:3000/api/login';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private _router: Router
+  ) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user);
@@ -25,5 +29,10 @@ export class AuthService {
 
   getToken() {
     return localStorage.getItem('token');
+  }
+
+  logouthUser() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/events']);
   }
 }
