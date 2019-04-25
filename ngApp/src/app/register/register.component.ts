2fda19e92c6ee49e1667e7b562f3330c94ab2e96
Yordan Yordanov <yordan.yordanov@dormakaba.com>
2019-04-25 14:04:11 +0300

    added registration service

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerUserData = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private _auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+    this._auth.registerUser(this.registerUserData)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
+    this.registerUserData = { email: '', password: '' }; // reset form input
   }
 
 }
