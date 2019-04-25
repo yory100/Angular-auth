d525363b1450c19c98dea3ba50858035643f0edc
Yordan Yordanov <yordan.yordanov@dormakaba.com>
2019-04-25 12:29:52 +0300

    added login

--- server/routes/api.js
+++ server/routes/api.js
@@ -28,4 +28,22 @@ router.post('/register', (req, res) => {
       })
 })
 
+router.post('/login', (req, res) => {
+      let userData = req.body
+
+      User.findOne({ email: userData.email }, (error, user) => {
+            if (error) {
+                  console.log(error)
+            } else {
+                  if (!user) {
+                        res.status(401).send('Invalid email')
+                  } else if (user.password !== userData.password) {
+                        res.status(401).send('Invalid password')
+                  } else {
+                        res.status(200).send(user)
+                  }
+            }
+      })
+})
+
 module.exports = router
